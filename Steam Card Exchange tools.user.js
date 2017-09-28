0653e4513f32672511558760f15cdf78d6610b68
Puffycheeses <noahheague@gmail.com>
2017-09-28 19:38:39 +1000

    Update Steam Card Exchange tools.user.js
    Added a span that tells you how many perfect matches it found

--- Steam Card Exchange tools.user.js
+++ Steam Card Exchange tools.user.js
@@ -3,15 +3,16 @@
 // @namespace    https://github.com/Puffycheeses/SteeamCardExchangeTools
 // @downloadURL  https://github.com/Puffycheeses/SteeamCardExchangeTools/raw/master/Steam%20Card%20Exchange%20tools.user.js
 // @updateURL    https://github.com/Puffycheeses/SteeamCardExchangeTools/raw/master/Steam%20Card%20Exchange%20tools.user.js
-// @version      1.0
+// @version      1.1
 // @description  Small tools to help improve the inventory search
 // @author       Puffycheeses
 // @match        *://www.steamcardexchange.net/index.php?inventory
 // @grant        none
 // ==/UserScript==
 $('body').append('<style>#settingsbox {background-color:#18191B;height:300px;width:200px;position:fixed;top:41px;left:0px;margin:0px;}</style>');
-$('body').append('<div id="settingsbox"><center><br>Card value<input type="number" id="cardVal"><br><br>Cards in set<input type="number" id="cardSet"><br><br>Best matches appear in green, Average matches appear in orange, Set matches appear in pink<br><br><br></center></div>');
+$('body').append('<div id="settingsbox"><center><br>Card value<input type="number" id="cardVal"><br><br>Cards in set<input type="number" id="cardSet"><br><br>Best matches appear in green, Average matches appear in orange, Set matches appear in pink<br><br><br>Perfect Matches: <span id="greenmatch">0</span><br></center></div>');
 window.setInterval(function(){
+    $('#greenmatch').html(0);
     var set = $('#cardSet').val();
     var value = $('#cardVal').val();
     var price = '';
@@ -35,6 +36,7 @@ window.setInterval(function(){
                 if(price == value){
                     $(row).parent().removeClass("odd");
                     $(row).parent().css('background-color','#00ff00');
+                    $('#greenmatch').html(parseInt($('#greenmatch').html(), 10)+1);
                 }
             }
         }

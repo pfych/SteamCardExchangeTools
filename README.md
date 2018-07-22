eee554d8da430d76b7b1db2d907fdbacbd9dd4cf
Noah <noahheague@gmail.com>
2018-07-22 15:57:31 +1000

    Fixed and cleaned code. Runs nicer now and looks better.

--- README.md
+++ README.md
@@ -1,25 +1,11 @@
 # Steam Card Exchange Tools
-Adds a more advanced search and highlighting, Its not the best code but It does improve the time it takes to find cards
+Adds a more advanced search and highlighting.
 
-[Click to install](https://github.com/Puffycheeses/SteeamCardExchangeTools/raw/master/Steam%20Card%20Exchange%20tools.user.js) - (Requires Tampermonkey )
+[Click to install](https://github.com/Puffycheeses/SteeamCardExchangeTools/raw/master/SCET.user.js) - (Requires Tampermonkey )
 
 # What does it do
 This script adds small box in the top left of the bots inventory on the site and allows you to highlight content by value and ammount.
 
-Green = Matches price, Matches card amount and there is no markup on the cards.  
-Orange = Matches price, Matches card amount and there is a markup on the cards.  
-Pink = Matches card amount.
-
-# How to use efficently 
-This script makes searching the bots inventory easier and more efficent. I am assuming you know how to earn credits on the bot    
-1. Make sure the script is installed
-2. Go to [The bots inventory](http://www.steamcardexchange.net/index.php?inventory)
-3. Change "Show 10 entries" to "Show 500 entries" (Do not do "all")
-4. Set the card value to what you want
-5. Set the card set to what you want
-6. Select the offer found (Make sure you have the right amount of credits)
-7. Right click on send offer and click open in new tab (Repeat for each card)
-8. Send offer (Repeat for each card)
-10. Wait for the cards to come through!
-
-I recomend setting value to 6 and set to 5 so it only costs 30 credits to get a full set.
+**Green** = Matches Price and Set Amount  
+**Orange** = Matches Price and Set Amount but there is a a price markup   
+**Red** = Matches Price and Set Amount but there is none in stock
---
+++ SCET.user.js
@@ -0,0 +1,48 @@
+// ==UserScript==
+// @name         SCET
+// @namespace    https://github.com/Puffycheeses/SteeamCardExchangeTools
+// @downloadURL  https://github.com/Puffycheeses/SteeamCardExchangeTools/raw/master/Steam%20Card%20Exchange%20tools.user.js
+// @updateURL    https://github.com/Puffycheeses/SteeamCardExchangeTools/raw/master/Steam%20Card%20Exchange%20tools.user.js
+// @version      2.0
+// @description  Small tools to help improve the inventory search
+// @author       Puffycheeses
+// @match        *://www.steamcardexchange.net/index.php?inventory
+// @grant        none
+// ==/UserScript==
+
+let $ = window.jQuery;
+$('body').prepend('<div style=\'display: inline-block; position: fixed; z-index: 999; background-color: #060707; width: 12%; margin-top: 21px; color: white\'><div style=\'padding: 10px;\'><div class=\'form-group\'><label for="exampleInputEmail1">Cards in Set</label><input style=\'width: 100%;\' class="form-control cardSet" aria-describedby="CardSet" placeholder="Cards in set"></div><div class=\'form-group\'><label for="exampleInputEmail1">Price of cards</label><input style=\'width: 100%;\' class="form-control carValue" aria-describedby="CardPrice" placeholder="Price of cards"></div><br><div style=\'width: 100%; background-color: white; color: black; cursor: pointer;\' class="refresh">Submit</div><br><span style=\'color: #72ff7a\'>Perfect match</span> <span class=\'perMatch\'>0</span><br><span style=\'color: #ffc264\'>One in stock</span> <span class=\'oneStock\'>0</span><br><span style=\'color: #ff3b4d\'>No in stock</span> <span class=\'noStock\'>0</span></div></div>')
+$('a:link').css("color", "white");
+$('a:visited').css("color", "white");
+$('.refresh').click(function () {
+    let set = $(".cardSet").val();
+    let value = $(".carValue").val();
+    $('tbody tr td').each(function () {
+        $( this ).parent().css("background-color", "");
+        if( $( this ).is(`:contains("(${set} of ${set} Cards)")`) && $( this ).is(`:not(:contains("1x"))`) && $(this).parent().find('td').eq(1).text() === value){
+            perfectMatch(this)
+        } else if ( $( this ).is(`:contains("(${set} of ${set} Cards)")`) && $( this ).is(`:not(:contains("0x"))`) && $(this).parent().find('td').eq(1).text() === value) {
+            oneStock(this)
+        } else if ( $( this ).is(`:contains("(${set} of ${set} Cards)")`) && $(this).parent().find('td').eq(1).text() === value) {
+            noStock(this)
+        }
+    })
+});
+
+function perfectMatch(body) { // Matches set and price
+    $( body ).parent().css('background-color','#72ff7a').css('color', 'white');
+    $( body ).parent().removeClass("odd")
+    $('.perMatch').html(parseInt($('.perMatch').html(), 10)+1);
+}
+
+function oneStock(body) { // Matches set and price but only one in stock
+    $( body ).parent().css('background-color','#ffc264').css('color', 'white');
+    $( body ).parent().removeClass("odd");
+    $('.oneStock').html(parseInt($('.oneStock').html(), 10)+1);
+}
+
+function noStock(body) { // Matches set and price but no in stock
+    $( body ).parent().css('background-color','#ff3b4d').css('color', 'white');
+    $( body ).parent().removeClass("odd");
+    $('.noStock').html(parseInt($('.noStock').html(), 10)+1);
+}
